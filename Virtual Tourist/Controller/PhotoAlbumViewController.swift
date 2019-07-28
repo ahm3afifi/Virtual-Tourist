0b2119dff475308db4b1ae76a5acbfc16fb769c5
Ahmed_Afifi <ahm3afifi@gmail.com>
2019-07-28 02:25:39 +0200

    created UI views

---
+++ Virtual Tourist/Controller/PhotoAlbumViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoAlbumViewController.swift
+//  Virtual Tourist
+//
+//  Created by Ahmed Afifi on 7/28/19.
+//  Copyright © 2019 Ahmed Afifi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+    
+}
